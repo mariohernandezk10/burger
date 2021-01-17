49ab9554d2e4f2a46176088339ea785bbd5b2270
Mario Hernandez <mariohernandezk10@gmail.com>
2021-01-17 17:06:20 -0600

    editing routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("/public"));
 
+
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
