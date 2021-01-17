e3e4757d75002ba1e2217640f4cc0e71fad21435
Mario Hernandez <mariohernandezk10@gmail.com>
2021-01-17 17:09:35 -0600

    fixed loading error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 
 // Parse application body
