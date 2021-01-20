91b4ffa4471d70ae55055d9d1bd86c388f096485
Mario Hernandez <mariohernandezk10@gmail.com>
2021-01-19 22:47:56 -0600

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+else connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
